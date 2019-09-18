ed5861ca7f647dbd7cc68c029f8a98754d8a5f21
mangesh <mvadkte@gmail.com>
2019-09-18 14:23:29 +0530

    Himalaya Web Application Initial Commit

---
+++ src/main/java/com/nit/himalaya/HimalayaBookSpringBootGitAdminAppApplication.java
@@ -0,0 +1,13 @@
+package com.nit.himalaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimalayaBookSpringBootGitAdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimalayaBookSpringBootGitAdminAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nit/himalaya/HimalayaBookSpringBootGitAdminAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nit.himalaya;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HimalayaBookSpringBootGitAdminAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
